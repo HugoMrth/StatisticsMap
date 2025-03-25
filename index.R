c962c0d38979c87d186627f045ffd1af4f610680
Hugo Marthinet <h.marthinet@gmail.com>
2025-03-25 11:19:58 +0100

    Add files via upload

--- index.R
+++ index.R
@@ -1,14 +1,11 @@
 library(networkD3)
-library(dplyr)
+library(tidyverse)
 library(openxlsx)
 
 # Import 
-DATA <- read.xlsx("C:/Users/hugo.marthinet/Downloads/StatisticsMap.xlsx")
+DATA <- read.xlsx("C:/Users/hugo.marthinet/Downloads/index.xlsx")
 
-data(MisLinks)
-data(MisNodes)
-head(MisLinks)
-head(MisNodes)
+
 
 Nodes <- data.frame(
   id = 0:(nrow(DATA)-1),
@@ -17,10 +14,22 @@ Nodes <- data.frame(
   size = 1
 )
 Nodes$size[!is.na(DATA$group)] <- diff(c(which(!is.na(DATA$group)), nrow(DATA)+1))
-Nodes$size[!is.na(DATA$name1)] <- diff(c(which(!is.na(DATA$name1)), nrow(DATA)+1))
-Nodes$size[!is.na(DATA$name2)] <- diff(c(which(!is.na(DATA$name2)), nrow(DATA)+1))
-Nodes$size[!is.na(DATA$name3)] <- diff(c(which(!is.na(DATA$name3)), nrow(DATA)+1))
-Nodes$size[!is.na(DATA$name4)] <- diff(c(which(!is.na(DATA$name4)), nrow(DATA)+1))
+
+condition <- is.na(DATA$group)
+values <- diff(c(which(!is.na(DATA$name1[condition])),nrow(DATA[condition,])+1))
+Nodes$size[!is.na(DATA$name1)] <- ifelse(values == 1, 1, values - 1)
+  
+condition <- is.na(DATA$group) & is.na(DATA$name1)
+values <- diff(c(which(!is.na(DATA$name2[condition])), nrow(DATA[condition,])+1))
+Nodes$size[!is.na(DATA$name2)] <- ifelse(values == 1, 1, values - 1)
+
+condition <- is.na(DATA$group) & is.na(DATA$name1) & is.na(DATA$name2)
+values <- diff(c(which(!is.na(DATA$name3[condition])), nrow(DATA[condition,])+1))
+Nodes$size[!is.na(DATA$name3)] <- ifelse(values == 1, 1, values - 1)
+
+condition <- is.na(DATA$group) & is.na(DATA$name1) & is.na(DATA$name2) & is.na(DATA$name3)
+values <- diff(c(which(!is.na(DATA$name4[condition])), nrow(DATA[condition,])+1))
+Nodes$size[!is.na(DATA$name4)] <- ifelse(values == 1, 1, values - 1)
 
 
 getParentNode <- function(child, parent) {
@@ -71,3 +80,44 @@ Links <- rbind(Links,
                  ),
                  value = 1
                ))
+
+
+
+interLinks <- cbind(
+  id = Nodes$id[!is.na(DATA$links)], 
+  links = DATA$links[!is.na(DATA$links)]
+  )
+interLinks <- rbind(
+  list_rbind(apply(interLinks[str_detect(interLinks[, 2], ", "),], 1, function(x) {
+    data.frame(id = x[1],
+               links = unlist(str_split(x[2], ", ")))
+  })),
+  interLinks[!str_detect(interLinks[, 2], ", "),]
+)
+interLinks <- left_join(
+  interLinks,
+  Nodes[, c("name", "id")],
+  by = c("links" = "name")
+)[, c(1, 3)]
+interLinks$value <- 1
+colnames(interLinks) <- c("source", "target", "value")
+interLinks$source <- as.numeric(interLinks$source)
+
+Links <- rbind(Links, interLinks) 
+
+
+forceNetwork(Links = Links, 
+             Nodes = Nodes, 
+             Source = "source",
+             Target = "target", 
+             Value = "value", 
+             NodeID = "name",
+             Group = "group", 
+             Nodesize = "size",
+             colourScale = JS("d3.scaleOrdinal(d3.schemeCategory20);"), 
+             radiusCalculation = "d.nodesize*2+6",
+             opacity = 1, 
+             opacityNoHover = 0.85,
+             charge = -1000,
+             fontSize = 100,
+             zoom = TRUE)
